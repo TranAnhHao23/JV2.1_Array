6205c4d057e1d11c30dbf90d55b811e52eb6ab33
TranAnhHao23 <Trananhhaonuce@gmail.com>
2021-12-06 23:44:03 +0700

    Add file

---
+++ src/CountSymbol.java
@@ -0,0 +1,25 @@
+import com.sun.javafx.iio.gif.GIFImageLoaderFactory;
+
+import java.util.Scanner;
+
+public class CountSymbol {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str;
+        String s;
+        int count = 0;
+        System.out.println("Nhập 1 chuỗi bất kỳ đi: ");
+        str = sc.nextLine();
+        System.out.println("Nhập ký tự muốn tìm: ");
+        s = sc.nextLine();
+
+        for (int i = 0; i < str.length(); i++) {
+            String kyTu = String.valueOf(str.charAt(i));
+            if (s.equals(kyTu)) {
+                count++;
+            }
+        }
+
+        System.out.println("Tổng số ký tự " + s + " trong chuỗi đã nhập = " + count);
+    }
+}
